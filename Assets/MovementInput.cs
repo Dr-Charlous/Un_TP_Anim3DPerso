c01a088ae68e73daca22ab8ee78107d380b934f6
Dr-Charlous <113095075+Dr-Charlous@users.noreply.github.com>
2023-04-06 06:31:37 +0200

    Attaque qui va mal

--- Assets/MovementInput.cs
+++ Assets/MovementInput.cs
@@ -1,3 +1,5 @@
+using DG.Tweening;
+using EzySlice;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +10,9 @@ public class MovementInput : MonoBehaviour
     public float RotationSpeed;
     public CharacterController Controller;
     public Animator Animator;
+    public GameObject PlaneCut;
+    public GameObject Sphere;
+    public LayerMask LayerCut;
 
     void Start()
     {
@@ -35,5 +40,67 @@ public class MovementInput : MonoBehaviour
 
         if (movementIntensity > 0.3f)
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), RotationSpeed/100);
+
+
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            PlaneCut.SetActive(true);
+
+            Time.timeScale = 0.2f;
+
+            Camera.main.DOKill();
+            Camera.main.DOFieldOfView(40, 0.3f);
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            PlaneCut.SetActive(false);
+
+            Time.timeScale = 1f;
+
+            Camera.main.DOKill();
+            Camera.main.DOFieldOfView(60, 0.1f);
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            PlaneCut.transform.eulerAngles += new Vector3(0, 0, -Input.GetAxis("Mouse X") * 5);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            SlicedHull hull = Sphere.Slice(PlaneCut.transform.position, PlaneCut.transform.up);
+            if (hull != null)
+            {
+                GameObject bootom = hull.CreateLowerHull(Sphere, null);
+                GameObject top = hull.CreateUpperHull(Sphere, null);
+                Destroy(Sphere);
+            }
+        }
+
+
+        Collider[] hits = Physics.OverlapBox(PlaneCut.transform.position, new Vector3(10, 0.1f, 10), PlaneCut.transform.rotation);
+
+        foreach (var item in hits)
+        {
+            SlicedHull hull = item.gameObject.Slice(PlaneCut.transform.position, PlaneCut.transform.up);
+            if (hull != null)
+            {
+                GameObject bootom = hull.CreateLowerHull(item.gameObject, null);
+                GameObject top = hull.CreateUpperHull(item.gameObject, null);
+                Destroy(Sphere);
+            }
+        }
+    }
+
+    void CreatePieceComponent(GameObject go)
+    {
+        go.layer = LayerMask.NameToLayer("destroyable");
+        Rigidbody rb = go.AddComponent<Rigidbody>();
+        MeshCollider collider = go.AddComponent<MeshCollider>();
+        collider.convex = true;
+
+        rb.AddExplosionForce(100, go.transform.position, 20);
     }
 }
